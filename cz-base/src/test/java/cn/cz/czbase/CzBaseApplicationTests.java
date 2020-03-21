8ba3963359c78fb0b3f850e7d3afe508162ce46d
xiaobo-hj <xiaobohj@163.com>
2020-03-21 22:14:39 +0800

    创建父项目模块  服务注册中心  base服务模块

---
+++ cz-base/src/test/java/cn/cz/czbase/CzBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cz.czbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CzBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
